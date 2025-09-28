4297a640fe7df0119937b70d43cef7604a66d636
priyankasunagar <priyankasunagar2007@gmail.com>
2025-09-28 12:09:50 +0530

    Add files via upload
    adding two numbers

---
+++ adding two numbers.py
@@ -0,0 +1,8 @@
+#program to add two numbers
+#taking input from user
+num1=float(input("enter first number:"))
+num2=float(input("enter second number:"))
+#adding the numbers
+sum=num1+num2
+#displaying the result
+print("the sum is:",sum)
