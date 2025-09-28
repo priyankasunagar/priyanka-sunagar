b09906abd82e598848d8330bf10e3a5dc6620f66
priyankasunagar <priyankasunagar2007@gmail.com>
2025-09-28 12:31:45 +0530

    Add files via upload
    coverting kelvin to celcius

---
+++ CONVERTING TEMPRATURE FROM KELVIN TO CELCIUS.py
@@ -0,0 +1,7 @@
+#program to convert kelvin to celcius
+#taking input from user
+kelvin=float(input("enter temprature in kelvin:"))
+#covresion formula
+celcius=kelvin-273.15
+#displaying the result
+print("the temprature in celcius :",celcius)
