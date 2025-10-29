d00e930dc774f537448aba5cd5b0569394114acf
priyankasunagar <priyankasunagar2007@gmail.com>
2025-10-29 18:50:36 +0530

    Add files via upload
    it is feels like computer is talking with us

---
+++ python robot.py
@@ -0,0 +1,13 @@
+import time
+print("hello there !")
+time.sleep(1)
+print("I am your friendly python robot")
+time.sleep(1)
+print("let me tell you a secret...")
+time.sleep(2)
+print("i can count faster than you")
+time.sleep(1)
+for i in range(1,6):
+    print(f"counting ...{i}")
+    time.sleep(0.5)
+print("done ! see? easy peasy")
